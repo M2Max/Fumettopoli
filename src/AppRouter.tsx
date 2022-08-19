6b09b35c6018b3d4c2d06e1e07dacec2596c1f27
Maximiliano Mamone <maximiliano.n.mamone@gmail.com>
2022-08-19 14:33:07 +0200

    Homepage da fare e finire

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Homepage } from "./components/Homepage/Homepage";
+import { Login } from "./components/Login/Login";
 
 function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/home" element={ <Homepage/>} /> 
+                <Route path="/home" element={ <Homepage/>} />
+                <Route path="/login" element={ <Login />} />
+                <Route path="/" element={<Navigate replace to="/home" />} />
             </Routes>
         </BrowserRouter>
     )
