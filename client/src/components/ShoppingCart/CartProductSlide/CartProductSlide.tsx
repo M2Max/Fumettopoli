7dbb4cda04d96a68dc1f456dec01cd2552cf3be9
Maximiliano <maximiliano.n.mamone@gmail.com>
2022-09-20 12:27:22 +0200

    Iniziato carrello

--- client/src/components/ShoppingCart/CartProductSlide/CartProductSlide.tsx
+++ client/src/components/ShoppingCart/CartProductSlide/CartProductSlide.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Slide2 from "../../../Resources/alice.jpg";
 
-import "./ProductSlide.css";
+import "./CartProductSlide.css";
 
-const ProductSlide = () => {
+interface productInformation {
+    productName: string;
+    quantity: number;
+    totalPrice: number;
+}
+
+const CartProductSlide = (props: productInformation) => {
     return (
         <div className="container product-slide text-center">
             <img src={Slide2} alt="" className="img-fluid"/>
-            <button className="add-to-cart btn mx-auto mt-5 normal-text">Add to cart</button>
+            <p className="add-to-cart btn mx-auto mt-5 normal-text">{props.productName} <br/> {props.quantity} <br/> {props.totalPrice} </p>
         </div>
     );
 }
 
-export default ProductSlide;
+export default CartProductSlide;
--- client/src/components/ShoppingCart/ShoppingCart.tsx
+++ client/src/components/ShoppingCart/ShoppingCart.tsx
@@ -1,14 +1,51 @@
+import { useEffect, useState } from "react";
 import useLoginStatus from "../../Hooks/useLoginStatus";
 import ProductSlide from "../SwiperSliders/ProductSlide/ProductSlide";
+import CartProductSlide from "./CartProductSlide/CartProductSlide";
 
 const ShoppingCart = () => {
     useLoginStatus();
+    const [response, setResponse] = useState(null);
+    const [error, setError] = useState('');
+    const [loading, setloading] = useState(true);
+
+    useEffect(() => {
+        cartFetch();
+    }, [])
 
     const cartFetch = () => {
         let user = sessionStorage.getItem("logged-user")
         if(user !== null)
             user = JSON.parse(user);
-        // Function that fetches data from database about the users cart
+        
+        console.log(user);
+        
+        
+        // const endopoint = "/auth/signin";
+        // const body = JSON.stringify({
+        //     id: user.id,
+        //     accessToken: user.accessToken,
+        // })
+    
+        // axios({
+        //     method: method,
+        //     url: endopoint,
+        //     headers: headers,
+        //     data: body
+        // }).then((res: any) => {
+        //     setResponse(res.data);
+        //     sessionStorage.setItem("logged-user", JSON.stringify(res.data));
+        //     setError('');
+        //     setloading(true);
+        // })
+        // .catch((err: any) => {
+        //     setError(err);
+        // })
+        // .finally(() => {
+        //     setloading(false);
+        //     if(sessionStorage.getItem("logged-user") !== null)
+        //         navigate("/home");
+        // });
     }
     
     return (
@@ -16,7 +53,7 @@ const ShoppingCart = () => {
             <div className="container w-50 mt-5">
                 <div className="row">
                     <div className="col-md-4">
-                        <ProductSlide/> {/* Substitute with cart slides */}
+                        <CartProductSlide productName="prodotto 1" quantity={10} totalPrice={50}/> {/* Substitute with cart slides */}
                     </div>
                     <div className="col-md-4">
                         <ProductSlide/>
